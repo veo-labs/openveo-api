912e0fa39b1b8a21616e15bdf8bd9b5d6a206c16
Maxime Béguin <maxime.beguin@veo-labs.com>
2016-05-13 17:19:42 +0200

    Remove taxonomies from openveo-api
    Taxonomies was used as a generic entity for all taxonomies, each taxonomy extending the notion of taxonomy present in openveo-api. Taxonomies are now moved to openveo-core. To perform CRUD operations on taxonomies, use routes exposed by openveo-core.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Module dependencies
 var path = require('path');
 
 // Set module root directory
@@ -17,10 +16,8 @@ module.exports.i18n = process.requireAPI('lib/i18n.js');
 module.exports.util = process.requireAPI('lib/util.js');
 module.exports.logger = process.requireAPI('lib/logger.js');
 module.exports.EntityModel = process.requireAPI('lib/models/EntityModel.js');
-module.exports.TaxonomyModel = process.requireAPI('lib/models/TaxonomyModel.js');
 module.exports.ContentModel = process.requireAPI('lib/models/ContentModel.js');
 module.exports.EntityProvider = process.requireAPI('lib/providers/EntityProvider.js');
-module.exports.TaxonomyProvider = process.requireAPI('lib/providers/TaxonomyProvider.js');
 module.exports.middlewares = process.requireAPI('lib/middlewares/index.js');
 module.exports.errors = process.requireAPI('lib/errors/index.js');
 module.exports.controllers = process.requireAPI('lib/controllers/index.js');
