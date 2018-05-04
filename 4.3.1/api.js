236eb6f5e67820a8eddfd874d586239bdfa11fad
Maxime Béguin <maxime.beguin@veo-labs.com>
2018-05-04 09:36:35 +0200

    Auto-generated documentation for version 5.0.0

--- 4.3.1/api.js
+++ 4.3.1/api.js
@@ -9,14 +9,21 @@ YUI.add("yuidoc-meta", function(Y) {
         "CAS2",
         "CAS3",
         "CasStrategy",
+        "ComponentExpression",
+        "ConfigExpression",
+        "ConstantExpression",
         "ContentController",
-        "ContentModel",
         "Controller",
         "Database",
+        "DirectiveExpression",
+        "ElementExpression",
         "EntityController",
-        "EntityModel",
         "EntityProvider",
-        "Model",
+        "Expression",
+        "FilterExpression",
+        "HttpController",
+        "InjectExpression",
+        "ModuleExpression",
         "MongoDatabase",
         "MultipartParser",
         "NotFoundError",
@@ -24,47 +31,47 @@ YUI.add("yuidoc-meta", function(Y) {
         "Plugin",
         "PluginApi",
         "Provider",
+        "ResourceFilter",
+        "RouteExpression",
         "STRATEGIES",
         "SocketController",
         "SocketNamespace",
         "SocketServer",
+        "Storage",
+        "ValueExpression",
         "disableCacheMiddleware",
         "factory",
         "fileSystem",
         "http-errors",
+        "imageProcessorMiddleware",
         "logRequestMiddleware",
         "logger",
+        "ngDpTask",
         "removeTask",
         "renameTask",
         "util"
     ],
     "modules": [
         "controllers",
-        "database",
         "emitters",
         "errors",
         "fileSystem",
         "grunt",
         "logger",
         "middlewares",
-        "models",
         "multipart",
         "passport",
         "plugin",
         "providers",
         "socket",
+        "storages",
         "util"
     ],
     "allModules": [
         {
             "displayName": "controllers",
             "name": "controllers",
-            "description": "Base controllers' stuff to be used by all controllers.\n\n    // Load module \"controllers\"\n    var controllers = require('@openveo/api').controllers;"
-        },
-        {
-            "displayName": "database",
-            "name": "database",
-            "description": "Databases implementations.\n\n    // Load module \"database\"\n    var database = require('@openveo/api').database;"
+            "description": "Controllers execute actions on routed messages.\n\nA controller performs controls on incoming parameters, executes the requested action and send a response to the\nclient.\n\n    // Load module \"controllers\"\n    var controllers = require('@openveo/api').controllers;"
         },
         {
             "displayName": "emitters",
@@ -97,11 +104,6 @@ YUI.add("yuidoc-meta", function(Y) {
             "description": "OpenVeo ExpressJS middlewares.\n\n    // Load module \"middlewares\"\n    var middlewares = require('@openveo/api').middlewares;"
         },
         {
-            "displayName": "models",
-            "name": "models",
-            "description": "Base models to be used by all models.\n\n    // Load module \"models\"\n    var models = require('@openveo/api').models;"
-        },
-        {
             "displayName": "multipart",
             "name": "multipart",
             "description": "All elements necessary to parse multipart requests.\n\n    // Load module \"multipart\"\n    var multipart = require('@openveo/api').multipart;"
@@ -119,12 +121,17 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "providers",
             "name": "providers",
-            "description": "Base providers' to be used by all providers.\n\n    // Load module \"providers\"\n    var providers = require('@openveo/api').providers;"
+            "description": "Providers store resources they define.\n\nA provider is the protector of the resources it defines. It communicates with its associated storage to manipulate\nits resources.\n\n    // Load module \"providers\"\n    var providers = require('@openveo/api').providers;"
         },
         {
             "displayName": "socket",
             "name": "socket",
             "description": "All elements necessary to create socket servers.\n\n    // Load module \"socket\"\n    var socket = require('@openveo/api').socket;"
+        },
+        {
+            "displayName": "storages",
+            "name": "storages",
+            "description": "Storages hold different ways of storing a resource.\n\nA storage is capable of performing CRUD (Create Read Update Delete) operations to manage a set of\nresources. A storage doesn't have any knowledge about the resource, it just stores it.\n\n    // Load module \"storages\"\n    var storage = require('@openveo/api').storages;"
         },
         {
             "displayName": "util",
